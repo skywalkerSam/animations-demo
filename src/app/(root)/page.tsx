29cdcd90cf97e12a7bcea400dca2a62f93d009fc
Starboy <samskywalker001@gmail.com>
2025-06-26 13:58:00 +0000

    initials

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import "~/styles/home.module.css";
+// import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Animations Demo</h1>
+    </main>
+  );
+}
