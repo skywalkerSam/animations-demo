0a9da86b973fa97e2c546702b7af2175a15aab21
Starboy <samskywalker001@gmail.com>
2025-06-28 14:25:05 +0000

    initials

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -2,7 +2,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main className="bg-gradient-to-b from-transparent via-sky-400 to-transparent">
+      <main className="bg-gradient-to-b from-transparent via-orange-400 to-transparent">
         {children}
       </main>
     </>
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,11 @@
-import "~/styles/home.module.css";
 // import Link from "next/link";
 
 export default function Home() {
   return (
     <main>
-      <h1>Animations Demo</h1>
+      <h1 className="flex-center min-h-screen font-bold sm:text-2xl md:text-4xl lg:text-5xl">
+        Animations Demo
+      </h1>
     </main>
   );
 }
