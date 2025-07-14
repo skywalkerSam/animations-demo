c5c702f56210808cb5687eba28b14c158f41d164
Starboy <samskywalker001@gmail.com>
2025-07-14 09:05:23 +0000

    skip type check

--- next.config.js
+++ next.config.js
@@ -5,6 +5,10 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  // Skip type check for reduced build times.)
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+};
 
 export default config;
