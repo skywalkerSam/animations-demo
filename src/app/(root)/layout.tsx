ff9ef28bb40d616c75f8840e08aa1b9b7f9e77ea
Starboy <samskywalker001@gmail.com>
2025-07-15 00:17:29 +0000

    Archives

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,7 @@
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <main>
-        {/* <main className="bg-gradient-to-b from-transparent via-sky-400 to-transparent"> */}
-        {children}
-      </main>
+      <main>{children}</main>
     </>
   );
 }
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,13 +1,8 @@
-// import Link from "next/link";
-
 import Hero from "./_components/hero";
 
 export default function Home() {
   return (
     <main>
-      {/* <h1 className="flex-center min-h-screen font-bold sm:text-2xl md:text-4xl lg:text-5xl">
-        Animations Demo
-      </h1> */}
       <Hero></Hero>
     </main>
   );
