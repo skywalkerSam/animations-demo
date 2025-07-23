4516a056254073d71dd930f6a88af42d57949d59
Starboy <samskywalker001@gmail.com>
2025-07-23 15:09:28 +0000

    initials

--- src/app/earth/_components/earth-3d/ExperienceEarth.tsx
+++ src/app/earth/_components/earth-3d/ExperienceEarth.tsx
@@ -13,24 +13,23 @@ export default function ExperienceEarth() {
   const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
 
   return (
-    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+    <Canvas camera={{ position: [0, 0, 10], fov: 30 }}>
+      <Sun />
       <Particles></Particles>
-      {/* deep blue ambient */}
-      <ambientLight intensity={0.2} color="#1a1a40" />
+      
       {/* Configure OrbitControls to disable panning and control zoom based on device type */}
       <OrbitControls
-        enablePan={false} // Prevents panning of the scene
+        // enablePan={false} // Prevents panning of the scene
         enableZoom={!isTablet} // Disables zoom on tablets
-        maxDistance={20} // Maximum distance for zooming out
-        minDistance={5} // Minimum distance for zooming in
-        minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
-        maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
+        maxDistance={30} // Maximum distance for zooming out
+        minDistance={3} // Minimum distance for zooming in
+        // minPolarAngle={Math.PI / 5} // Minimum angle for vertical rotation
+        // maxPolarAngle={Math.PI / 2} // Maximum angle for vertical rotation
       />
 
       <Suspense fallback={null}>
-        <Sun />
         <group
-          scale={isMobile ? 0.7 : 1}
+          scale={isMobile ? 0.7 : 3}
           position={[0, -3.5, 0]}
           rotation={[0, -Math.PI / 4, 0]}
         >
--- src/app/earth/_components/earth-3d/Sun.tsx
+++ src/app/earth/_components/earth-3d/Sun.tsx
@@ -2,40 +2,16 @@ import * as THREE from "three";
 
 const Sun = () => (
   <>
-    {/* lamp's light */}
-    <spotLight
-      position={[2, 5, 6]}
-      angle={0.15}
-      penumbra={0.5}
-      intensity={90}
-      color="white"
-    />
-    {/* bluish overhead lamp */}
-    <spotLight
-      position={[4, 5, 4]}
-      angle={0.3}
-      penumbra={0.5}
-      intensity={40}
-      color="#4cc9f0"
-    />
-    {/* purplish side fill */}
-    <spotLight
-      position={[-3, 5, 5]}
-      angle={0.4}
-      penumbra={0.5}
-      intensity={60}
-      color="#9d4edd"
-    />
-    {/* area light for soft moody fill */}
-    <primitive
-      object={new THREE.RectAreaLight("#a259ff", 8, 3, 2)}
-      position={[1, 3, 4]}
-      rotation={[-Math.PI / 4, Math.PI / 4, 0]}
-      intensity={15}
-    />
-    {/* subtle point light for atmospheric tone */}
-    <pointLight position={[0, 1, 0]} intensity={10} color="#7209b7" />
-    <pointLight position={[1, 2, -2]} intensity={10} color="#0d00a4" />
+    {/* Indigo (#4B0082), Blue (#0000FF), Electric Green (#00FF00), Yellow (#FFFF00), Web Orange (#FFA500) and Red (#FF0000). */}
+    <ambientLight intensity={10} color="#4B0082"></ambientLight>
+    <ambientLight intensity={1} color="#0000FF"></ambientLight>
+    <ambientLight intensity={1} color="#00FF00"></ambientLight>
+    <ambientLight intensity={1} color="##FFFF00"></ambientLight>
+    <ambientLight intensity={1} color="#FFA500"></ambientLight>
+    <ambientLight intensity={1} color="#FF0000"></ambientLight>
+
+    <directionalLight position={[0, 0, 5]} intensity={10} color={"#4B0082"}></directionalLight>
+    <directionalLight position={[0, 0, -5]} intensity={10} color={"#4B0082"}></directionalLight>
   </>
 );
 
